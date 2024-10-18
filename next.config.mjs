2207d9e9b10bd674707d963774086b6ea9d3e5fd
ekiost <choonkeatling@icloud.com>
2024-10-19 00:30:03 +0800

    Github Page

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,5 @@ import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/nextjs-github-pages",
-  images: {
-    unoptimized: true,
-  },
+
 };
