9fb9fc8b754136b0706aa928b68ee16993035283
ekiost <choonkeatling@icloud.com>
2024-10-19 01:13:52 +0800

    Close navbar after link is clicked

--- src/components/sections/header/cozy/nav/body/body.tsx
+++ src/components/sections/header/cozy/nav/body/body.tsx
@@ -1,68 +1,73 @@
-import { motion } from 'framer-motion';
-import { Link as LinkType } from '@/types/link';
+import {motion} from 'framer-motion';
+import {Link as LinkType} from '@/types/link';
 import styles from './style.module.scss';
-import { blur, translate } from '../../anim';
+import {blur, translate} from '../../anim';
 import Link from 'next/link';
 
 interface SelectedLink {
-  isActive: boolean;
-  index: number;
+    isActive: boolean;
+    index: number;
 }
 
 interface BodyProps {
-  links: LinkType[];
-  selectedLink: SelectedLink;
-  setSelectedLink: (selectedLink: SelectedLink) => void;
-  setIsActive: (isActive: boolean) => void;
+    links: LinkType[];
+    selectedLink: SelectedLink;
+    setSelectedLink: (selectedLink: SelectedLink) => void;
+    setIsActive: (isActive: boolean) => void;
 }
 
 export default function Body({
-  links,
-  selectedLink,
-  setSelectedLink,
-  setIsActive
-}: BodyProps) {
-  const getChars = (word: string) => {
-    let chars: JSX.Element[] = [];
-    word.split('').forEach((char, i) => {
-      chars.push(
-        <motion.span
-          custom={[i * 0.02, (word.length - i) * 0.01]}
-          variants={translate}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          key={char + i}
-        >
-          {char}
-        </motion.span>
-      );
-    });
-    return chars;
-  };
+                                 links,
+                                 selectedLink,
+                                 setSelectedLink,
+                                 setIsActive
+                             }: BodyProps) {
+    const getChars = (word: string) => {
+        let chars: JSX.Element[] = [];
+        word.split('').forEach((char, i) => {
+            chars.push(
+                <motion.span
+                    custom={[i * 0.02, (word.length - i) * 0.01]}
+                    variants={translate}
+                    initial="initial"
+                    animate="enter"
+                    exit="exit"
+                    key={char + i}
+                >
+                    {char}
+                </motion.span>
+            );
+        });
+        return chars;
+    };
 
-  return (
-    <div className={styles.body}>
-      {links.map((link, index) => {
-        const { title, href, target } = link;
+    return (
+        <div className={styles.body}>
+            {links.map((link, index) => {
+                const {title, href, target} = link;
 
-        return (
-          <Link key={`l_${index}`} href={href} target={target}>
-            <motion.p
-              onMouseOver={() => setSelectedLink({ isActive: true, index })}
-              onMouseLeave={() => setSelectedLink({ isActive: false, index })}
-              variants={blur}
-              animate={
-                selectedLink.isActive && selectedLink.index !== index
-                  ? 'open'
-                  : 'closed'
-              }
-            >
-              {getChars(title)}
-            </motion.p>
-          </Link>
-        );
-      })}
-    </div>
-  );
+                return (
+                    <Link
+                        key={`l_${index}`}
+                        href={href}
+                        target={target}
+                        onClick={() => setIsActive(false)} // Close navbar on click
+                    >
+                        <motion.p
+                            onMouseOver={() => setSelectedLink({isActive: true, index})}
+                            onMouseLeave={() => setSelectedLink({isActive: false, index})}
+                            variants={blur}
+                            animate={
+                                selectedLink.isActive && selectedLink.index !== index
+                                    ? 'open'
+                                    : 'closed'
+                            }
+                        >
+                            {getChars(title)}
+                        </motion.p>
+                    </Link>
+                );
+            })}
+        </div>
+    );
 }
