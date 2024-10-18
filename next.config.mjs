1d4fc3b311e594af0ccddac9fa00ffa0263a745d
ekiost <choonkeatling@icloud.com>
2024-10-19 00:20:02 +0800

    Github Page

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,6 @@ import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000']
-    }
-  },
   output: "export",
   basePath: "/nextjs-github-pages",
   images: {
